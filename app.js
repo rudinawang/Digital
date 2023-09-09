13fb58998ce60b2e1bad2f26a81a41714e3927df
rudinawang <rudnawang07@gmail.com>
2023-09-09 17:07:57 +0545

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express"); // requring or importing express package
+const app = express(); // storing it in app, app vannay variable throughout use
+
+// ejs use garna lako ho, k k chahinay ho env set gardey
+app.set("view enigne", "ejs");
+
+// slash(/) ma gayo vanay k dekhaunay
+// re Request paxi Response hunai paryo
+app.get("/", (req, res) => {
+  // views folder vitra ko ejs file lai track garxa
+  res.render("home.ejs");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about.ejs");
+});
+
+app.get("/contact", (req, res) => {
+  res.send("From contact page");
+});
+
+//port number is like room number where our project is working or running
+app.listen(3000, () => {
+  console.log("Nodejs Project has started on port 3000");
+});
